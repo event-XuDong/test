fe22dc168ba46c79fc2e44621118e37c0e308f47
徐东 <18515139150@163.com>
2015-12-22 11:13:48 +0800

    rrr
    项目

---
+++ rrr/rrr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rrr
+//
+//  Created by 徐东 on 15/12/22.
+//  Copyright © 2015年 徐东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rrr/rrr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rrr
+//
+//  Created by 徐东 on 15/12/22.
+//  Copyright © 2015年 徐东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
